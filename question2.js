3375fea1022b3e55c747e18e3a086f5a3f4c0fa3
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-05-15 11:46:16 +0530

    question2 added

---
+++ question2.js
@@ -0,0 +1,16 @@
+let requests = [["excavator", 50000], ["bulldozer", 70000]];
+let sellers = [["excavator", 45000], ["bulldozer", 68000], ["excavator", 48000]];
+let a=50000,b=70000;
+for(let i=0;i<sellers.length;i++){
+    // console.log(sellers[i]);
+        if(sellers[i][0]==='excavator'){
+            a=Math.min(a,sellers[i][1])
+        }
+         if(sellers[i][0]==='bulldozer'){
+            b=Math.min(b,sellers[i][1])
+        }
+    
+}
+// console.log(a,b);
+let arr=[a,b];
+console.log(arr);
